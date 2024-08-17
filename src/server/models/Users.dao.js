11dcecf84a301e96bae27d40e7dcdfb4dcdfd0d4
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-08-17 15:38:44 -0400

    user.dao

--- src/server/models/Users.dao.js
+++ src/server/models/Users.dao.js
@@ -2,8 +2,18 @@ const db = require('../database/db');
 const { compareSync } = require('../../utils/bcrypt');
 
 const verifyCredentials = async (username, password) => {
-    const [user] = await db('SELECT * FROM usuarios WHERE username = $1', [username]);
-    return compareSync(password, user.password) ? [user] : [];
+    try {
+        const result = await db.query('SELECT * FROM usuarios WHERE username = $1', [username]);
+        const user = result.rows[0];
+        if (user && compareSync(password, user.password)) {
+            return [user];
+        } else {
+            return [];
+        }
+    } catch (error) {
+        console.error('Error al verificar las credenciales:', error);
+        throw error;
+    }
 };
 
 module.exports = {

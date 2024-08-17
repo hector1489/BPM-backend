6ae9615ee75aecfdd9c3a9934e82384233af404d
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-08-17 16:10:43 -0400

    ruta users

--- src/server/models/Events.dao.js
+++ src/server/models/Events.dao.js
@@ -1,23 +1,23 @@
 const db = require('../database/db');
 const { encrypt } = require('../../utils/bcrypt');
 
-const findUsuarios = async () => await db('SELECT * FROM usuarios;');
+const findUsuarios = async () => await db('SELECT * FROM users;');
 
-const findUsuarioByEmail = async (email) => await db('SELECT * FROM usuarios WHERE email = $1;', [email]);
+const findUsuarioByEmail = async (email) => await db('SELECT * FROM users WHERE email = $1;', [email]);
 
 const createUsuario = async ({ username, email, role,  password }) => {
-  const query = 'INSERT INTO usuarios (username, email, role,  password) VALUES ($1, $2, $3, $4) RETURNING *;';
+  const query = 'INSERT INTO users (username, email, role,  password) VALUES ($1, $2, $3, $4) RETURNING *;';
   const values = [username, email, role, encrypt(password)];
   return await db(query, values);
 };
 
 const updateUsuario = async (id, { username, email, role,  password }) => {
-  const query = 'UPDATE usuarios SET username = $2, email = $3, role = $4, password = $5 WHERE id = $1 RETURNING *;';
+  const query = 'UPDATE users SET username = $2, email = $3, role = $4, password = $5 WHERE id = $1 RETURNING *;';
   const values = [username, email, role,  password];
   return await db(query, values);
 };
 
-const deleteUsuario = async (id) => await db('DELETE FROM usuarios WHERE id = $1 RETURNING *;', [id]);
+const deleteUsuario = async (id) => await db('DELETE FROM users WHERE id = $1 RETURNING *;', [id]);
 
 module.exports = {
   findUsuarios,

90152c7a282d5630da700ac6a429d71332ddf7de
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-08-29 10:49:19 -0400

    nodemailer config

--- src/server/models/Desviaciones.dao.js
+++ src/server/models/Desviaciones.dao.js
@@ -6,12 +6,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    type: 'OAuth2',
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    pass: process.env.EMAIL_PASS
   },
 });
 

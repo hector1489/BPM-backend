2edeba606b4f7fbc3b61028db3d457c66523b970
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-10-14 19:37:49 -0300

    devolver errores

--- src/server/routes/tableDetailsRoutes.js
+++ src/server/routes/tableDetailsRoutes.js
@@ -4,37 +4,47 @@ const { createTablaDetail, getAllTablaDetails, deleteTablaDetail } = require('..
 
 
 router.get('/tabla-details', async (req, res) => {
-    try {
-        const result = await getAllTablaDetails();
-        res.status(200).json(result.rows);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener los detalles' });
-    }
+  try {
+    const result = await getAllTablaDetails();
+    res.status(200).json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener los detalles' });
+  }
 });
 
 
 router.post('/tabla-details', async (req, res) => {
-    try {
-        const data = req.body;
-        const result = await createTablaDetail(data);
-        res.status(201).json(result.rows[0]);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al crear el detalle' });
-    }
+  try {
+    const data = req.body;
+    const result = await createTablaDetail(data);
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error('Error en el backend al crear el detalle:', error);
+
+
+    res.status(500).json({
+      error: 'Error al crear el detalle',
+      message: error.message,
+      stack: error.stack,
+      name: error.name,
+      code: error.code || 'UNKNOWN'
+    });
+  }
 });
 
+
 router.delete('/tabla-details/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const result = await deleteTablaDetail(id);
-        if (result.rowCount > 0) {
-            res.status(200).json({ message: 'Registro eliminado exitosamente', detail: result.rows[0] });
-        } else {
-            res.status(404).json({ error: 'Registro no encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Error al eliminar el registro' });
+  try {
+    const { id } = req.params;
+    const result = await deleteTablaDetail(id);
+    if (result.rowCount > 0) {
+      res.status(200).json({ message: 'Registro eliminado exitosamente', detail: result.rows[0] });
+    } else {
+      res.status(404).json({ error: 'Registro no encontrado' });
     }
+  } catch (error) {
+    res.status(500).json({ error: 'Error al eliminar el registro' });
+  }
 });
 
 module.exports = router;
